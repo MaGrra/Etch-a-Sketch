88f11bc53f2714c58615ebc939fce008bcf772dd
MaGrra <mrti.grra@gmail.com>
2023-03-01 17:33:19 +0200

    Add main functions
    Add rainbow mode
    Add reset button
    Add color picker
    *not combined yet

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 const container = document.querySelector('.container');
 let size = prompt('Iedo cipar');
-let sheet = document.createElement('style')
+let sheet = document.createElement('style');
+let colorId = '';
 
 
 
+/*Create a grid*/
 function create() {
     let square = 100 / size;
     for (let i = 0; i < (size ** 2); i++) {
@@ -14,14 +16,35 @@ function create() {
         container.appendChild(field);
 }
 };
+/*Coloring functions*/
 function toBlack(e) {
     this.classList.add('toBlack');
+}
+function rainbow(e) {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    this.setAttribute(`style`, `background: #${randomColor}`);
+}
+function staticColor(e) {
+    document.getElementById('colorPicker').value;
+    this.setAttribute(`style`, `background: ${colorPicker.value}`)
+    console.log(colorId);
 } 
+function reset(e) {
+    fields.forEach(field => field.setAttribute('style', ''));
+}
 create();
 
 
 const fields = document.querySelectorAll('.field');
+const colorPicker = document.querySelector('#colorPicker');
+const btnReset = document.querySelector('#btnReset');
 
-fields.forEach(field => field.addEventListener('mouseover', toBlack));
+btnReset.addEventListener('click', reset)
+
+
+
+
+
+fields.forEach(field => field.addEventListener('mouseover', rainbow));
 
 
