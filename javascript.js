34e57d1aee0af751fea01572e528132bf2db4032
MaGrra <mrti.grra@gmail.com>
2023-03-03 16:46:24 +0200

    Add size adjust
    Plus some stying

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 const container = document.querySelector('.container');
-let size = prompt('Iedo cipar');
+const slider = document.querySelector('.slider');
+let size = slider.value;
 let sheet = document.createElement('style');
 let colorValue = '#000000';
+
 
 
 
@@ -15,48 +17,61 @@ function create() {
         document.body.appendChild(sheet);
         container.appendChild(field);
 }
+    const fields = document.querySelectorAll('.field');
+    fields.forEach(field => field.addEventListener('mouseover', color));
+    btnGrid.addEventListener('click', grid);
+    btnReset.addEventListener('click', reset);
+    function reset(e) {
+        fields.forEach(field => field.setAttribute('style', ''));
+    }
+    function grid(e) {
+        fields.forEach(field=>field.classList.toggle('innerBorder'));
+    }
+
 };
 /*Coloring functions*/
-function reset(e) {
-    fields.forEach(field => field.setAttribute('style', ''));
-}
-function grid(e) {
-    fields.forEach(field=>field.classList.toggle('innerBorder'));
-}
+
 function randomColor(e) {
     colorValue = "#" + (Math.floor(Math.random()*16777215).toString(16));
     return colorValue;
 }
 function darker(e) {
-    
+
 }
 function color(e) {
     this.setAttribute('style', `background: ${colorValue}`)
 };
+function setSize(e) {
+    size = slider.value;
+    console.log(size);
+    return size;
+}
 
 create();
-
-const fields = document.querySelectorAll('.field');
-const colorPicker = document.querySelector('#colorPicker');
-const btnReset = document.querySelector('#btnReset');
 const btnColor = document.querySelector('#btnColor');
-const btnGrid = document.querySelector('#btnGrid');
-
-btnGrid.addEventListener('click', grid);
-btnReset.addEventListener('click', reset);
+const colorPicker = document.querySelector('#colorPicker');
 
 
-fields.forEach(field => field.addEventListener('mouseover', color));
 
 
+slider.addEventListener('input', (e) => {
+    size = slider.value;
+    container.innerHTML = '';
+    create();
+    console.log(size);
+})
 colorPicker.addEventListener('input', (e) => {
      window.removeEventListener("mouseover", randomColor);
      colorValue = colorPicker.value;
+     btnColor.classList.remove('btnOn');
+     colorPicker.setAttribute('style', 'transition: 0.5s; border: solid 4px rgb(202, 15, 15);');
      return colorValue;
 }); 
 btnColor.addEventListener('click', (e) => {
     window.addEventListener('mouseover', randomColor)
+    btnColor.classList.add('btnOn');
+    colorPicker.setAttribute('style', 'transition: 0.5s;');
     return "#" + colorValue
 });
 
-
+console.log(slider.value);
