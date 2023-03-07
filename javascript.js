419905d11831d42933e453fcbd3099e66ddc70dc
MaGrra <mrti.grra@gmail.com>
2023-03-07 18:58:02 +0200

    Design and functionality done v1

--- javascript.js
+++ javascript.js
@@ -4,7 +4,9 @@ let size = slider.value;
 let sheet = document.createElement('style');
 let colorValue = '#8f00ac';
 filter = 100;
-
+var mouseIsDown = false
+container.addEventListener('mousedown', function(){mouseIsDown = true})
+window.addEventListener('mouseup', function(){mouseIsDown = false})
 
 
 
@@ -23,10 +25,15 @@ function create() {
     btnGrid.addEventListener('click', grid);
     btnReset.addEventListener('click', reset);
     function reset(e) {
+        click.currentTime = 0;
+        click.play();
         fields.forEach(field => field.setAttribute('style', ''));
     }
     function grid(e) {
+        click.currentTime = 0;
+        click.play();
         fields.forEach(field=>field.classList.toggle('innerBorder'));
+        btnGrid.classList.toggle('btnOn');
     }
 
 };
@@ -38,7 +45,12 @@ function randomColor(e) {
 }
 
 function color(e) {
-    this.setAttribute('style', `background: ${colorValue}`);
+    if (mouseIsDown) {
+        this.setAttribute('style', `background: ${colorValue}`);
+        draw.play();
+    } else draw.pause();
+    
+    
 };
 function setSize(e) {
     size = slider.value;
@@ -53,29 +65,54 @@ const screen = document.querySelector('.screen');
 const btnDarker =document.querySelector('#btnDark');
 const beachBtn = document.querySelector('.beachBtn');
 const body = document.querySelector('body');
+const click = document.querySelector('.click');
+const draw = document.querySelector('.draw');
+const travel = document.querySelector('.travel');
+const mute = document.querySelector('#btnMute');
+
 
 body.classList.toggle('beach');
 beachBtn.addEventListener('click', (e) => {
+    travel.volume = 0.2;
+    travel.currentTime = 0;
+    travel.play();
+    click.play();
     body.classList.toggle('beach');
 })
 
 
 
 
+mute.addEventListener('click', (e) => {
+    click.currentTime = 0;
+    click.play();
+    btnMute.classList.toggle('btnOn');
+    if (btnMute.classList.contains('btnOn')) {
+        draw.muted = true;
+        travel.muted = true;
+        click.muted = true;
+    } else draw.muted = false,
+            click.muted = false,
+            travel.muted = false;
+});
 
 slider.addEventListener('input', (e) => {
     size = slider.value;
     container.innerHTML = '';
     create();
+    click.play();
+    btnGrid.classList.remove('btnOn');
 })
 colorPicker.addEventListener('input', (e) => {
+        click.play();
      window.removeEventListener("mouseover", randomColor);
      colorValue = colorPicker.value;
      btnColor.classList.remove('btnOn');
-     colorPicker.setAttribute('style', 'transition: 0.5s; border: solid 4px rgb(202, 15, 15);');
+     colorPicker.setAttribute('style', `transition: 0.5s; border: solid 3px ${colorValue};`);
      return colorValue;
 }); 
 btnColor.addEventListener('click', (e) => {
+    click.play();
     window.addEventListener('mouseover', randomColor)
     btnColor.classList.add('btnOn');
     colorPicker.setAttribute('style', 'transition: 0.5s;');
